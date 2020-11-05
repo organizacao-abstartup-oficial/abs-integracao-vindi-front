c3654b24ad87f58acf9d100ba03b8b82ad79510a
mr.dougladmorais23@gmail.com <mr.dougladmorais23@gmail.com>
2020-11-05 13:14:28 -0300

    Update end points (serveless)

--- src/Data/endPoints.js
+++ src/Data/endPoints.js
@@ -1,4 +1,4 @@
-export const API = 'https://api-planos.abstartups.com.br/';
+export const API = 'https://api-planos.abstartups.com.br/https://rx10kvzj0h.execute-api.us-east-1.amazonaws.com/dev/';
 
 export const AuthHeather = {
   Headers : {
