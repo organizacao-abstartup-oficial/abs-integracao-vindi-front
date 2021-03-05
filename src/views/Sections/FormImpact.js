0cb441ed5eeb859d7b125323a36b6a6063ecd2e5
abstartups-oficial <tecnologia@abstartups.com.br>
2021-03-05 16:48:56 -0300

    fix impact

--- src/views/Sections/FormImpact.js
+++ src/views/Sections/FormImpact.js
@@ -148,7 +148,8 @@ export default function FormImpact() {
 
   async function handleSubscription(cnpj){
     await api.get(`validate/${cnpj.replace(/\D/g, '')}`).then(response => {
-      if (response.data.status === 401 || response.data.body.customer[0].metadata.impact !== "sim"){
+      console.log(response.data)
+      if (response.data.status_code === 400 || response.data.body.customer.impact !== "sim"){
         history.push('/')
       } else {
         setValidaCnpj(true);
