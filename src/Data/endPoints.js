c46432fdf2ddda4177c343a2c93967e29714ae72
mrdouglasmorais <mr.douglasmorais23@gmail.com>
2021-03-08 19:12:20 -0300

    End point update

--- src/Data/endPoints.js
+++ src/Data/endPoints.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  // baseURL: 'https://4xuzm4m60a.execute-api.us-east-1.amazonaws.com/dev/'
-  baseURL: 'http://localhost:3000/dev/'
+  baseURL: 'https://4xuzm4m60a.execute-api.us-east-1.amazonaws.com/dev/'
 })
 
 export default api;
